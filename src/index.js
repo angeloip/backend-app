14c8722695b0d14ac4dab06c0a4abb8ec291e6de
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2023-03-29 11:17:49 -0500

    29/03/23

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://192.168.1.2:3001"],
     credentials: true
   })
 );
